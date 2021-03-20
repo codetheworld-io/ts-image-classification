5835c564ebb8b0b8a63498dae8f128c401155339
hoangdv <hoang.dv@outlook.com>
2021-03-20 22:35:42 +0900

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es2021": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 12
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
   },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "ignorePatterns": [
-    "dist"
-  ],
-  "rules": {
-    "no-console": 2,
-  }
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['dist'],
+  rules: {
+    'no-console': 2,
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'no-multi-spaces': 'error',
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-  roots: [
-    "<rootDir>/src"
-  ],
+  roots: ['<rootDir>/src'],
   testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.d.ts",
-    "!**/node_modules/**",
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
   ],
   globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
     },
   },
-}
+};
